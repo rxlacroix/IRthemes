8a99e30e7d67a57702ebd14045efad91f40683c2
Romain Lacroix <romain.lacroix@free.fr>
2024-04-09 16:25:17 +0200

    import_fonts

---
+++ R/import_IRfonts.R
@@ -0,0 +1,19 @@
+#' Fonction pour importer les différentes polices utilisées dans les thèmes de l'institut rhodanien
+#'
+#'
+#'
+#' @param
+#' @export
+#' @examples
+#'
+#'
+
+import_IRfonts <- function (){
+  import_candara()
+  import_r()
+  import_zs()
+  import_rm()
+  import_out()
+  import_spl()
+}
+
